6cf74146acde9b8f77aed0bfe0ba9af4d215e660
Zeztron <pateltharsh@gmail.com>
2020-05-14 10:13:10 -0400

    added signin logic

--- auth/src/routes/signin.ts
+++ auth/src/routes/signin.ts
@@ -1,7 +1,11 @@
 import { Router, Request, Response } from "express";
-import { body, validationResult } from 'express-validator';
+import { body } from 'express-validator';
+import jwt from 'jsonwebtoken';
 
-import { RequestValidationError } from '../errors/request-validation-error';
+import { User } from '../models/user';
+import { Password } from '../services/password';
+import { validateRequest } from '../middlewares/validate-request';
+import { BadRequestError } from '../errors/bad-request-error';
 
 const router = Router();
 
@@ -13,10 +17,30 @@ router.post("/api/users/signin", [
     .trim()
     .notEmpty()
     .withMessage('You must supply a password')
-], (req: Request, res: Response) => {
-  const errors = validationResult(req);
+], validateRequest, async (req: Request, res: Response) => {
+  const { email, password } = req.body;
 
-  if (!errors.isEmpty()) throw new RequestValidationError(errors.array());
+  const existingUser = await User.findOne({ email });
+  if (!existingUser) throw new BadRequestError('Invalid credentials.');
+
+  const passwordsMatch: boolean = await Password.compare(existingUser.password, password);
+
+  if (!passwordsMatch) throw new BadRequestError('Invalid credentials.');
+
+  const userJwt = jwt.sign(
+    {
+      id: existingUser.id,
+      email: existingUser.email,
+    },
+    process.env.JWT_KEY!
+  );
+
+  // Sote it on session
+  req.session = {
+    jwt: userJwt,
+  };
+
+  res.status(200).send(existingUser);
 });
 
 export { router as signinRouter };
