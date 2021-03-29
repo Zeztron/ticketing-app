3d0bd08089f196b9ebd983bba950fa7ff0d0658f
Zeztron <pateltharsh@gmail.com>
2021-03-29 14:12:06 -0400

    added signin, signout, and header

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,12 @@
 import buildClient from '../api/build-client';
 
 const LandingPage = ({ currentUser }) => {
-  console.log(currentUser);
-  return <h1>Landing Page</h1>;
+  return <h1>Welcome, {currentUser ? currentUser.email : 'user'}</h1>;
 };
 
 LandingPage.getInitialProps = async (context) => {
-  const client = buildClient(context);
-  const { data } = await client.get('/api/users/currentuser');
+  const apiClient = buildClient(context);
+  const { data } = await apiClient.get('/api/users/currentuser');
   return data;
 };
 
