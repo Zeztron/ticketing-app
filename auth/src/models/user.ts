be53f07364135dabed7975593036ab364340d3d0
Zeztron <pateltharsh@gmail.com>
2021-03-30 12:02:28 -0400

    defined Ticket model

--- auth/src/models/user.ts
+++ auth/src/models/user.ts
@@ -9,50 +9,51 @@ interface UserAttrs {
 }
 
 // An interface that describes the properties
-// That a User model has
-interface UserModel extends mongoose.Model<UserDoc> {
-  build(attrs: UserAttrs): UserDoc;
-}
-
-// An interface that describes the properties
 // that a User Document has
 interface UserDoc extends mongoose.Document {
   email: string;
   password: string;
 }
 
-const userSchema = new mongoose.Schema({
-  email: {
-    type: String,
-    required: true
-  },
-  password: {
-    type: String,
-    required: true
-  }
-}, {
-  toJSON:{
-    transform(doc, ret) {
-      ret.id = ret._id;
-      delete ret.id;
-      delete ret.password;
-      delete ret.__v;
-    }
-  }
-});
+// An interface that describes the properties
+// That a User model has
+interface UserModel extends mongoose.Model<UserDoc> {
+  build(attrs: UserAttrs): UserDoc;
+}
 
-userSchema.pre('save', async function(done) {
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret.id;
+        delete ret.password;
+        delete ret.__v;
+      },
+    },
+  }
+);
+
+userSchema.pre('save', async function (done) {
   if (this.isModified('password')) {
     const hashedPassword = await Password.toHash(this.get('password'));
     this.set('password', hashedPassword);
-  };
+  }
 
   done();
 });
 
-userSchema.statics.build = (attrs: UserAttrs) => {
-  return new User(attrs);
-};
+userSchema.statics.build = (attrs: UserAttrs) => new User(attrs);
 
 const User = mongoose.model<UserDoc, UserModel>('User', userSchema);
 
