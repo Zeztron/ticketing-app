df9bf564b40ab53d42ee310762d5fb76c49aa144
Zeztron <pateltharsh@gmail.com>
2021-03-30 13:25:17 -0400

    added get all tickets test and route

--- tickets/src/app.ts
+++ tickets/src/app.ts
@@ -9,6 +9,7 @@ import { errorHandler, NotFoundError, currentUser } from '@hpgittix/common';
 // Routes
 import { createTicketRouter } from './routes/new';
 import { showTicketRouter } from './routes/show';
+import { indexTicketRouter } from './routes/index';
 
 const app = express();
 app.use(json());
@@ -24,6 +25,7 @@ app.use(currentUser);
 // Implementing Routes
 app.use(createTicketRouter);
 app.use(showTicketRouter);
+app.use(indexTicketRouter);
 
 // Implementing Middlewares
 app.use(errorHandler);
---
+++ tickets/src/routes/__test__/index.test.ts
@@ -0,0 +1,19 @@
+import request from 'supertest';
+import { app } from '../../app';
+
+const createTicket = (title: string, price: number) => {
+  return request(app).post('/api/tickets').set('Cookie', global.signin()).send({
+    title,
+    price,
+  });
+};
+
+it('can fetch a list of tickets', async () => {
+  await createTicket('ticket 1', 20);
+  await createTicket('ticket 2', 30);
+  await createTicket('ticket 3', 40);
+
+  const response = await request(app).get('/api/tickets').send().expect(200);
+
+  expect(response.body.length).toEqual(3);
+});
