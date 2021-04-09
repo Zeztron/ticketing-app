2d4a09209736999d55b139da452490569d0b226c
Zeztron <pateltharsh@gmail.com>
2021-04-09 09:37:45 -0400

    updated to OrderCancelledEvent in common module

--- orders/src/events/publishers/order-cancelled-publisher.ts
+++ orders/src/events/publishers/order-cancelled-publisher.ts
@@ -1,5 +1,5 @@
-import { Publisher, OrderCancelled, Subjects } from '@hpgittix/common';
+import { Publisher, OrderCancelledEvent, Subjects } from '@hpgittix/common';
 
-export class OrderCancelledPublisher extends Publisher<OrderCancelled> {
+export class OrderCancelledPublisher extends Publisher<OrderCancelledEvent> {
   subject: Subjects.OrderCancelled = Subjects.OrderCancelled;
 }
