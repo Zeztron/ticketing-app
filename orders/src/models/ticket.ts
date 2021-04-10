983d0284e28392a9314805bf2ba2c94a98506695
Zeztron <pateltharsh@gmail.com>
2021-04-10 08:22:27 -0400

    created success test cases for ticket listener events

--- orders/src/models/ticket.ts
+++ orders/src/models/ticket.ts
@@ -17,6 +17,10 @@ export interface TicketDoc extends mongoose.Document {
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
+  findByEvent(event: {
+    id: string;
+    version: number;
+  }): Promise<TicketDoc | null>;
 }
 
 const ticketSchema = new mongoose.Schema(
@@ -51,6 +55,12 @@ ticketSchema.statics.build = (attrs: TicketAttrs) => {
     ...rest,
   });
 };
+
+ticketSchema.statics.findByEvent = (event: { id: string; version: number }) =>
+  Ticket.findOne({
+    _id: event.id,
+    version: event.version - 1,
+  });
 
 // Run query to look at all orders. Find an order where the ticket
 // is the ticket we just found *and* the orders status is *not* cannclled.
