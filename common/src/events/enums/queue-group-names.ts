17282d2d7bde14c50eaf6e81d60f6a3b044981ce
Zeztron <pateltharsh@gmail.com>
2021-04-11 16:33:15 -0400

    Updates

--- common/src/events/enums/queue-group-names.ts
+++ common/src/events/enums/queue-group-names.ts
@@ -2,4 +2,5 @@ export enum queueGroupNames {
   ORDERS_SERVICE = 'orders-service',
   TICKETS_SERVICE = 'tickets-service',
   PAYMENTS_SERVICE = 'payments-service',
+  EXPIRATION_SERVICE = 'expiration-service',
 }
