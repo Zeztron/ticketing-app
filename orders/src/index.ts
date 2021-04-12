51abdb98b2615ef6b50e28d0b3b511428f855810
Zeztron <pateltharsh@gmail.com>
2021-04-12 08:06:30 -0400

    created listener for expiration complete and tests

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -4,6 +4,7 @@ import { DatabaseConnectionError } from '@hpgittix/common';
 import { natsWrapper } from './nats-wrapper';
 import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
 import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
+import { ExpirationCompleteListener } from './events/listeners/expiration-complete-listener';
 
 const PORT = 3000;
 
@@ -37,6 +38,7 @@ const start = async () => {
 
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
+    new ExpirationCompleteListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI!, {
       useNewUrlParser: true,
