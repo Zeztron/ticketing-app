b8d05fc7e8bec60e5d2195eab3a12c5f10500008
Zeztron <pateltharsh@gmail.com>
2021-04-12 07:26:19 -0400

    Updates

--- common/src/events/interfaces/ExpirationCompleteEvent.ts
+++ common/src/events/interfaces/ExpirationCompleteEvent.ts
@@ -1,6 +1,6 @@
 import { Subjects } from '../enums/subjects';
 
-export interface ExipirationCompleteEvent {
+export interface ExpirationCompleteEvent {
   subject: Subjects.ExpirationComplete;
   data: { orderId: string };
 }
