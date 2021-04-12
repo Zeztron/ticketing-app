8f13fd18d6678a70bfe634781f55281f8c657d69
Zeztron <pateltharsh@gmail.com>
2021-04-12 07:32:49 -0400

    created publisher for expiration

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -5,10 +5,22 @@ import {
   queueGroupNames,
 } from '@hpgittix/common';
 import { Message } from 'node-nats-streaming';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupNames.EXPIRATION_SERVICE;
 
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    const delay = new Date(data.expiresAt).getTime() - new Date().getTime();
+
+    await expirationQueue.add(
+      {
+        orderId: data.id,
+      },
+      { delay }
+    );
+
+    msg.ack();
+  }
 }
