51e1520e9f8e1b9f3871176bc22446554db4dec4
Zeztron <pateltharsh@gmail.com>
2021-04-12 09:55:43 -0400

    initial setup for payments service

---
+++ payments/src/app.ts
@@ -0,0 +1,31 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+// Middlewares and Errors
+import { errorHandler, NotFoundError, currentUser } from '@hpgittix/common';
+
+// Routes
+
+const app = express();
+app.use(json());
+app.set('trust proxy', true);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test',
+  })
+);
+app.use(currentUser);
+
+// Implementing Routes
+
+// Implementing Middlewares
+app.use(errorHandler);
+
+app.all('*', async (req, res) => {
+  throw new NotFoundError();
+});
+
+export { app };
