32b286d2790cf70f22bfffd3834f2b1b7c45f5f8
Zeztron <pateltharsh@gmail.com>
2021-04-13 10:20:24 -0400

    scaffolded a form to purchase a ticket

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,13 +1,9 @@
-import buildClient from '../api/build-client';
-
 const LandingPage = ({ currentUser }) => {
   return <h1>Welcome, {currentUser ? currentUser.email : 'user'}</h1>;
 };
 
-LandingPage.getInitialProps = async (context) => {
-  const apiClient = buildClient(context);
-  const { data } = await apiClient.get('/api/users/currentuser');
-  return data;
+LandingPage.getInitialProps = async (context, client, currentUser) => {
+  return {};
 };
 
 export default LandingPage;
